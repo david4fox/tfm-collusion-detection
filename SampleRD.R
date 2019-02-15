be3c10a2a2fab837f0086dfe4befb49b6b5f3318
David <chato4fox@hotmail.com>
2019-02-15 01:19:52 +0100

    umberOfBids is now random
    El número de pujas para cada uno de los contratos es aleatorio entre limites fijos

--- SampleRD.R
+++ SampleRD.R
@@ -1,22 +1,26 @@
 ## Input
-numberOfBids <- 20
-numberOfContracts <- 1000
-numberOfBreaks <- 50
+numberOfBidsMin <- 20
+numberOfBidsMax <- 25
+numberOfContracts <- 10000
+numberOfBreaks <- 1000
 
 ## Main function
-funtionSampleRD <- function(numberOfBids, numberOfContracts, numberOfBreaks){
+funtionSampleRD <- function(numberOfBidsMin, numberOfBidsMax, numberOfContracts, numberOfBreaks){
   listOfSamples <- list()
   sampleRD <- data.frame()
   for(i in c(1:numberOfContracts)){
-    listOfSamples[i] <- list(sort(sample(50:100, numberOfBids, replace=TRUE)))
-    sampleRD[i,1] <- (listOfSamples[[i]][2]-listOfSamples[[i]][1])/sd(listOfSamples[[i]][2:numberOfBids])
+    aux <- sample(numberOfBidsMin:numberOfBidsMax,1)
+    listOfSamples[i] <- list(sort(sample(c(sample(10:70,1):sample(80:150,1)), aux, replace=TRUE)))
+    sampleRD[i,1] <- (listOfSamples[[i]][2]-listOfSamples[[i]][1])/sd(listOfSamples[[i]][2:aux])
   }
          
   sampleRD <- sort(sampleRD[,1], decreasing = TRUE)
   
-  hist(sampleRD, main = "Distribution", breaks = numberOfBreaks, border = "orange", col = "yellow")
-  return(sampleRD)
+  hist(sampleRD, main = "Distribution", breaks = numberOfBreaks, border = "orange", col = "yellow", xlim = c(0,1))
 }
 
 ## Function
-funtionSampleRD(numberOfBids, numberOfContracts, numberOfBreaks)
+funtionSampleRD(numberOfBidsMin, numberOfBidsMax, numberOfContracts, numberOfBreaks)
+
+
+
