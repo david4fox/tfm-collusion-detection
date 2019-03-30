ec1ea0db9a3be31bc8facce0224d81935b5723c3
David <chato4fox@hotmail.com>
2019-03-30 15:08:18 +0100

    Diferent colours for non competitive and competitive sistuations

--- 2Dgraphs.R
+++ 2Dgraphs.R
@@ -1,5 +1,6 @@
 # Libraries
 library("plotly")
+library("dplyr")
 
 #Function
 graphic2Dfuntion <- function(FirstFirm, SecondFirm, BidData){
@@ -28,15 +29,40 @@ graphic2Dfuntion <- function(FirstFirm, SecondFirm, BidData){
     comparisonTable[count,5] <- min(Bids)
     comparisonTable[count,6] <- (comparisonTable[count,2]-comparisonTable[count,5])/(comparisonTable[count,4]-comparisonTable[count,5])
     comparisonTable[count,7] <- (comparisonTable[count,3]-comparisonTable[count,5])/(comparisonTable[count,4]-comparisonTable[count,5])
+    if((comparisonTable[count,6]>=0.5&comparisonTable[count,7]==0)|(comparisonTable[count,7]>=0.5&comparisonTable[count,6]==0)|(comparisonTable[count,6]>=0.5&comparisonTable[count,7]>=0.5)){
+      comparisonTable[count,8] <- "Non Competitive"
+    }else{
+      comparisonTable[count,8] <- "Competitive"
+    }
+    if(comparisonTable[count,8]=="Competitive"){
+      comparisonTable[count,9] <- comparisonTable[count,7]
+      comparisonTable[count,10] <- NA
+    }else{
+      comparisonTable[count,9] <- NA
+      comparisonTable[count,10] <- comparisonTable[count,7]
+    }
+    
   }
-  colnames(comparisonTable) <- c("Contracts", FirstFirm, SecondFirm, "MaxBid", "MinBid",paste0(FirstFirm,"_Nomalized"),paste0(SecondFirm,"_Nomalized"))
-  graph <- plot_ly(data = comparisonTable, x = comparisonTable[,6], y = comparisonTable[,7], type = "pointcloud")
+  colnames(comparisonTable) <- c("Contracts", FirstFirm, SecondFirm, "MaxBid", "MinBid",paste0(FirstFirm,"_Nomalized"),paste0(SecondFirm,"_Nomalized"),"CompetitiveZones","Competitive","NonCompetitive")
+  
+  graph <- plot_ly(data = comparisonTable, x = comparisonTable[,6], y = comparisonTable[,9], type = "scatter",mode = "markers" , name="Competitive",
+                   marker = list(color = 'rgb(100, 150, 255)',line = list(color = 'rgb(0, 0, 255)', width = 0.5))) %>%
+    layout(title = "Normalized Graph", titlefont = list(size = 30,color = 'rgb(47, 47, 147)'),
+           yaxis = list(linecolor = toRGB("black"),color = toRGB("black"),title = FirstFirm, range = c(-0.05,1.05), titlefont = list(size = 18,color = 'rgb(0, 0, 0)'), tickfont = list(size = 12,color = 'rgb(0, 0, 0)')),
+           xaxis = list(linecolor = toRGB("black"),color = toRGB("black"),title = SecondFirm, titlefont = list(size = 18,color = 'rgb(0, 0, 0)'), tickfont = list(size = 12,color = 'rgb(0, 0, 0)')),
+           margin = list(t = 75, b = 50,r = 10,l = 80), plot_bgcolor = 'rgb(255, 255, 255)', paper_bgcolor  = 'rgb(186, 186, 186)', barmode = 'group', bargap = 10,
+           legend = list(x = 0.18, y = 1.08,orientation = 'h')) %>%
+    add_trace(y= comparisonTable[,10],type = "scatter",mode = "markers", name="Non competitive",marker = list(color = 'rgb(255,50,50)',line = list(color = 'rgb(255,10,10)', width = 0.5)))
+  
+  comparisonTable<<-comparisonTable
   graph <<- graph
-  #return(graph)
+  return(graph)
 }
 
 ## Graph
 FirstFirm <- "Firm1"
-SecondFirm <- "Firm8"
-
+SecondFirm <- "Firm4"
+#BidData <- Bid_data
 graphic2Dfuntion(FirstFirm, SecondFirm, Bid_data)
+
+
